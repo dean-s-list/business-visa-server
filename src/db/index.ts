c3b338e15e1f00d8dd100fc4f9989f6a14dc72cb
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-07-29 20:53:28 +0530

    feat: add custom fetch fn on db for older node

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,12 @@ import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
 import env from "../env/index.js";
+import { fetch } from "undici";
 
 // create the connection
 const connection = connect({
     url: env.DB_URL,
+    fetch,
 });
 
 const db = drizzle(connection);
